02ec01fa75e3f7fdb84b536f4d4ad16233776aac
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-18 15:55:09 -0600

    create a welcome page

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,25 +1,74 @@
-import React, { Component } from 'react';
-import Nav from '../Nav/Nav';
-import "./SignIn.css"
+import React, { Component } from "react";
+import Nav from "../Nav/Nav";
+import "./SignIn.css";
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
 class SignIn extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            username: "",
-            password: ""
-        }
-    }
-    render() {
-        return(
-            <div>
-                <Nav/>
-                <p>SignIn Component</p>
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
+
+  changeHandler = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  login = (event) => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    axios
+      .post("/auth/login", { username, password })
+      .then((res) => {
+        this.props.loginUser(res.data);
+        this.props.history.push("/profile");
+      })
+      .catch((error) => console.log(error, "Could Not log in at this time"));
+  };
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <div className="signIn-container">
+        <Nav />
+        <div className="signIn-content">
+          <p className="page-title-content">Welcome Back!</p>
+          <form className="form-content" onSubmit={this.login}>
+            <input
+              className="signIn-input-box"
+              placeholder="Username"
+              type="text"
+              name="username"
+              value={username}
+              onChange={(event) => this.changeHandler(event)}
+            />
+            <input
+              className="signIn-input-box"
+              placeholder="Password"
+              type="password"
+              name="password"
+              value={password}
+              onChange={(event) => this.changeHandler(event)}
+            />
+            <div className="form-btns">
+              <input className="form-blue-btn" type="submit" value="Sign In" />
+              <input className="form-blue-btn" type="button" value="Cancel" />
             </div>
-        )
-    }
-
+            <p>
+          Don't already have an account?{" "}
+          <Link to="/register"><span className="register-btn-landing">Register Here</span></Link>
+        </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
