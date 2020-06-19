9fe15a9790c3b065053452083401c16973a4bd60
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-19 11:21:05 -0600

    create register user

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
+import './Register.css'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Register extends Component {
     constructor() {
@@ -10,14 +13,76 @@ class Register extends Component {
             password: ""
         }
     }
+
+    changeHandler = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value,
+        });
+      };
+    
+      register = () => {
+        const { username, password } = this.state;
+        axios
+          .post("/auth/register", { username, password })
+          .then((res) => {
+            this.props.loginUser(res.data);
+            this.props.history.push("/SignIn");
+          })
+          .catch((error) => {
+            alert(error, "Could not log in");
+          });
+      };
+
+
     render() {
-        return(
-            <div>
-                <Nav/>
-                <p>Register Component</p>
+        const { username, password } = this.state;
+        return (
+          <div className="register-container">
+            <Nav />
+            <div className="register-content">
+              <p className="page-title-content">Ready to strengthen your relationship</p>
+              <form className="form-content" onSubmit={this.login}>
+                <input
+                  className="register-input-box"
+                  placeholder="Username"
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={(event) => this.changeHandler(event)}
+                />
+                <input
+                  className="register-input-box"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(event) => this.changeHandler(event)}
+                />
+                <div className="form-btns">
+                  <input 
+                    className="form-blue-btn" 
+                    type="button"
+                    onClick={() => this.register()} 
+                    value="Register" />
+                  <Link to="/">
+                  <input
+                    className="form-blue-btn"
+                    type="button"
+                    value="Back"
+                  />
+                  </Link>
+                </div>
+                <p>
+                  Already have an account?{" "}
+                  <Link to="/SignIn">
+                    <span className="register-btn-landing">Sign in Here</span>
+                  </Link>
+                </p>
+              </form>
             </div>
-        )
-    }
+          </div>
+        );
+      }
 
 }
 
