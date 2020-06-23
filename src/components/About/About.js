d3cc8bfd7548d94f9aca52eb4753bc844e72b8da
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-23 16:35:56 -0600

    create a contact page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,21 +1,37 @@
-import React from 'react';
-import Nav from '../Nav/Nav';
-import './About.css';
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./About.css";
 
 function About() {
-    return(
-        <div className="about-container">
-            <Nav/>
-            <div className="about-content">
-                <p className="about-title">About</p>
-                <img className="about-main-pic" src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FHeart%20Taken%20logo.png?v=1592502714640" alt="Heart Taken logo"/>
-                <p className="paragraph-content"> With the rise of social media dating applications, and new ways for people to find that perfect someone to date, life seemed complete. However, relationships take work. While these apps help you find that someone, how can they help keep that someone? That's where HeartTaken comes into play <br/>
-                HeartTaken was founded in the summer of 2020. It started out as a project for fun, and turned into much more than that. With dedicated time and attention our team has uncovered a range of goals that will help you build a stronger relationship. These goals are based around 5 core love languages they include the following: words of affirmation, gifts, quality time, acts of service, and touch. <br/> 
-                We guarantee if you take the time each day to do something special for the one you love, you will say with confidence your "Heart is Taken."</p>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="about-container">
+      <Nav />
+      <div className="about-content">
+        <p className="about-title">About</p>
+        <img
+          className="about-main-pic"
+          src="https://cdn.glitch.com/875fcc3a-ee91-4d48-806c-d5b121d9c21c%2FHeart%20Taken%20logo.png?v=1592502714640"
+          alt="Heart Taken logo"
+        />
+        <p className="about-paragraph-content">
+          {" "}
+          With the rise of social media dating applications, and new ways for
+          people to find that perfect someone to date, life seemed complete.
+          However, relationships take work. While these apps help you find that
+          someone, how can they help keep that someone? That's where HeartTaken
+          comes into play <br />
+          HeartTaken was founded in the summer of 2020. It started out as a
+          project for fun, and turned into much more than that. With dedicated
+          time and attention our team has uncovered a range of goals that will
+          help you build a stronger relationship. These goals are based around 5
+          core love languages they include the following: words of affirmation,
+          gifts, quality time, acts of service, and touch. <br />
+          We guarantee if you take the time each day to do something special for
+          the one you love, you will say with confidence your "Heart is Taken."
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import Nav from '../Nav/Nav';
+import React from "react";
+import Nav from "../Nav/Nav";
+import "./Contact.css";
 
 function Contact() {
-    return(
-        <div>
-            <Nav/>
-            <p>Contact Component</p>
-        </div>
-    )
+  return (
+    <div className="contact-container">
+      <Nav />
+      <div className="contact-content">
+        <p className="contact-title">Get In Touch</p>
+        <p className="contact-paragraph">
+          We are always looking for ways to improve our user experience. If you
+          have any additional feedback or suggestions on how we can make this
+          application any better, please feel free to let us know.
+        </p>
+        <form className="contact-form-container">
+          <input className="message-field" 
+                 placeholder="Message"
+                 name="message"
+                 type="text"
+                 />
+          <div>
+            <input className="email-field" 
+                   placeholder="Email Address"
+                   name="email"
+                   type="email"
+                   />
+            <input className="name-field" 
+                   placeholder="Full Name"
+                   name="fullname" 
+                   type="text"
+                   />
+            <button className="contact-send-btn">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
